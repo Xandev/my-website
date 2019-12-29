521d946d93ca57c0716cdcd5be6e826e0b172951
Radek Mariowski <rmariowski@gmail.com>
2019-12-29 20:10:09 +0100

    Fix http-equiv meta tag

--- components/HtmlHead/HtmlHead.tsx
+++ components/HtmlHead/HtmlHead.tsx
@@ -10,7 +10,7 @@ const HtmlHead: FC<IndexHeadProps> = (props: IndexHeadProps) => {
     <Head>
       <title>{props.title}</title>
       <meta charSet="utf-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
   );
