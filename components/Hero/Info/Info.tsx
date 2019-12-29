1104a437d768879412bce0bdd50e83ffb71e8d08
Radek Mariowski <rmariowski@gmail.com>
2019-12-29 20:12:37 +0100

    Add dummy Info component for Hero

---
+++ components/Hero/Info/Info.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+
+const Info: FC = () => {
+  return (
+    <>
+      <h1>Welcome!</h1>
+      <h3>
+        I'm <strong>Radosław Mariowski</strong>, a <em>Full Stack Developer</em>
+        .
+      </h3>
+      <h3>Bio goes there</h3>
+    </>
+  );
+};
+
+export default Info;
