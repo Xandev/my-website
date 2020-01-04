18e4c90167ba288a660b74f7dc52b2b1c8d9c739
Radek Mariowski <rmariowski@gmail.com>
2020-01-04 12:48:39 +0100

    Add mail button icon

--- components/Hero/Social/Social.tsx
+++ components/Hero/Social/Social.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from "react";
 import { faGithubSquare, faTwitterSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
 import SocialItem from "./Item/SocialItem";
 
 const Social: FC = () => {
   return (
     <>
       <ul className="social">
+        <SocialItem href="mailto:radek@mariowski.pl" icon={faEnvelopeSquare} />
         <SocialItem href="https://github.com/Xandev" icon={faGithubSquare} />
         <SocialItem href="https://twitter.com/RMariowski" icon={faTwitterSquare} />
         <SocialItem href="https://www.linkedin.com/in/rados%C5%82aw-mariowski-4a1465ba/" icon={faLinkedin} />
