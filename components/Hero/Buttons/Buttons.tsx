4986d5a74e22d7d526737ad7c2d55ebad3e9a25e
Radek Mariowski <rmariowski@gmail.com>
2020-01-18 19:21:52 +0100

    Use css module for buttons component

--- components/Hero/Buttons/Buttons.tsx
+++ components/Hero/Buttons/Buttons.tsx
@@ -1,36 +1,15 @@
 import { FC } from "react";
+import styles from "./Buttons.module.css"
 
 const Buttons: FC = () => {
   return (
     <>
-      <a href="/about" className="button" title="About Me" aria-label="About Me">
+      <a href="/about" className={styles.button} title="About Me" aria-label="About Me">
         About Me
       </a>
-      <a href="/blog" className="button" title="Blog" aria-label="Blog">
+      <a href="/blog" className={styles.button} title="Blog" aria-label="Blog">
         Blog
       </a>
-
-      <style jsx={true}>{`
-        .button {
-          display: inline-block;
-          width: 100px;
-          padding: 12px 0;
-          margin: 0 5px;
-          border: 2px solid #337ab7;
-          border-radius: 3px;
-          color: #337ab7;
-          font-weight: 600;
-          line-height: 1;
-          cursor: pointer;
-          text-decoration: none;
-          user-select: none;
-        }
-
-        .button:hover {
-          background-color: #337ab7;
-          color: #fff;
-        }
-      `}</style>
     </>
   );
 };
