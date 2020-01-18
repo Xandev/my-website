36f4f844ec20fe7ede41c1b54d8c28681399259b
Radek Mariowski <rmariowski@gmail.com>
2020-01-18 19:32:09 +0100

    Use css module for social component

--- components/Hero/Social/Social.tsx
+++ components/Hero/Social/Social.tsx
@@ -2,28 +2,20 @@ import React, { FC } from "react";
 import { faGithubSquare, faTwitterSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
 import SocialItem from "./Item/SocialItem";
+import styles from "./Social.module.css"
 
 const Social: FC = () => {
   return (
-    <>
-      <ul className="social">
-        <SocialItem href="mailto:radek@mariowski.pl" icon={faEnvelopeSquare} title="Mail To" />
-        <SocialItem href="https://github.com/Xandev" icon={faGithubSquare} title="GitHub" />
-        <SocialItem href="https://twitter.com/RMariowski" icon={faTwitterSquare} title="Twitter" />
-        <SocialItem
-          href="https://www.linkedin.com/in/rados%C5%82aw-mariowski-4a1465ba/"
-          icon={faLinkedin}
-          title="LinkedIn"
-        />
-      </ul>
-
-      <style jsx={true}>{`
-        .social {
-          padding: 0;
-          list-style: none;
-        }
-      `}</style>
-    </>
+    <ul className={styles.list}>
+      <SocialItem href="mailto:radek@mariowski.pl" icon={faEnvelopeSquare} title="Mail To" />
+      <SocialItem href="https://github.com/Xandev" icon={faGithubSquare} title="GitHub" />
+      <SocialItem href="https://twitter.com/RMariowski" icon={faTwitterSquare} title="Twitter" />
+      <SocialItem
+        href="https://www.linkedin.com/in/rados%C5%82aw-mariowski-4a1465ba/"
+        icon={faLinkedin}
+        title="LinkedIn"
+      />
+    </ul>
   );
 };
 
