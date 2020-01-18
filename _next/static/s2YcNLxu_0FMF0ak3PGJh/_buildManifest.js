2e8aa46e1e7631f2b236feb8f96d79f3c4ddb23f
Xandev <Xandev@users.noreply.github.com>
2020-01-18 18:21:22 +0000

    Automated deployment: Sat Jan 18 18:21:22 UTC 2020 3eb841a530f2a14d1770fd8310c1b86bfcf00324

--- _next/static/s2YcNLxu_0FMF0ak3PGJh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b]}}("static\u002Fchunks\u002Fa9a7754c.cffe7e9d692e041c871b.js","static\u002Fchunks\u002Fcb1608f2.8e66296a5b4c4bb7fe53.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
