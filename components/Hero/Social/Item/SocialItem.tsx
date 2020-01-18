9691026a29a09e559291e0dc726823d767d4c149
Radek Mariowski <rmariowski@gmail.com>
2020-01-18 19:30:48 +0100

    Remove useless react fragment in social item

--- components/Hero/Social/Item/SocialItem.tsx
+++ components/Hero/Social/Item/SocialItem.tsx
@@ -11,13 +11,11 @@ interface SocialItemProps {
 
 const SocialItem: FC<SocialItemProps> = (props: SocialItemProps) => {
   return (
-    <>
-      <li className={styles.item}>
-        <a className={styles.link} href={props.href} target="_blank" title={props.title} aria-label={props.title}>
-          <FontAwesomeIcon icon={props.icon} />
-        </a>
-      </li>
-    </>
+    <li className={styles.item}>
+      <a className={styles.link} href={props.href} target="_blank" title={props.title} aria-label={props.title}>
+        <FontAwesomeIcon icon={props.icon} />
+      </a>
+    </li>
   );
 };
 
